a45c5fd515730ed7ccb977b7eb178c04808e830e
Sam Costanzo <samcostanzo25@gmail.com>
2019-04-19 12:24:26 -0400

    Got it!!!

--- routes/index.js
+++ routes/index.js
@@ -17,16 +17,18 @@ router.get('/about', (req, res) => {
 
 // Dynamic projects route to render each project
 router.get('/project/:id', (req, res) => {
-    res.render('project', {
-        title: projects[req.params.id].project_name,
-        description: projects[req.params.id].description,
-        technologies: projects[req.params.id].technologies,
-        livelink: projects[req.params.id].live_link,
-        githublink: projects[req.params.id].github_link,
-        imageURLS: projects[req.params.id].image_urls
-    });
-
-
+    if(req.params.id >= projects.length){
+        return res.redirect('/');
+    } else {
+        res.render('project', {
+            title: projects[req.params.id].project_name,
+            description: projects[req.params.id].description,
+            technologies: projects[req.params.id].technologies,
+            livelink: projects[req.params.id].live_link,
+            githublink: projects[req.params.id].github_link,
+            imageURLS: projects[req.params.id].image_urls
+        });
+    }
 });
 
 
